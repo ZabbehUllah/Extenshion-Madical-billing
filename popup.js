235ce35b4d1b3f2e8b1fab6d19f6e60f7a3972a5
ZabbehUllah <120080824+ZabbehUllah@users.noreply.github.com>
2025-05-03 12:13:18 -0700

    Update popup.js
    moo form fill tin and also some filling changes

--- popup.js
+++ popup.js
@@ -1,18 +1,66 @@
 
-  document.getElementById("fillBtn").addEventListener("click", () => {
-    const value = document.getElementById("fieldValue").value;
-  
+  document.getElementById("tinfillBtn").addEventListener("click", () => {
+    const value = document.getElementById("tinfieldValue").value;
+    
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
         func: (val) => {
           const input = document.getElementById("tin");
+          const radio = document.getElementById("terms");
+          const btn = document.getElementById("sign-in");
           if (input) {
             input.value = val;
-            input.style.border = "2px solid green"; // visual feedback
           }
+          if (radio) radio.checked = true;
+          if (btn) btn.click();
         },
         args: [value]
       });
     });
+  });
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const pgurl = tabs[0].url;
+      
+  
+      // For exact match
+      if (pgurl === "https://www31.mutualofomaha.com/ProviderAccess/") {
+        show("Sign-MOO");
+      } 
+      else if (pgurl === "https://www31.mutualofomaha.com/ProviderAccess/claimSearch") {
+        show("Detail-MOO");
+      }
+      else if (pgurl === "https://www31.mutualofomaha.com/ProviderAccess/eligibilitySearch") {
+      
+      }
+      else {
+        show("match-other");
+      }
+
+
+
+
+       
+      // For partial match (e.g., contains '123')
+      /*else if (url.includes("123")) {
+      show("match-123");
+      }*/
+      
+    });
+  
+    function show(id) {
+      document.getElementById(id).classList.remove("hidden");
+    }
+  });
+
+
+  document.getElementById("openSite").addEventListener("click", () => {
+  
+    const url = document.getElementById("siteSelect").value;
+  if (url) {
+    chrome.tabs.create({ url });
+  }
   });
