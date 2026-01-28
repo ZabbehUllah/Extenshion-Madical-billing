ca2307c713807b3cc9fd1cf7fb9c6a4090173bd9
ZabbehUllah <120080824+ZabbehUllah@users.noreply.github.com>
2026-01-28 08:33:18 -0800

    Comment out webRequest listener and manifest details

--- background.js
+++ background.js
@@ -1,13 +1,16 @@
+/*chrome.webRequest.onBeforeRequest.addListener(
+  async (details) => {
+    const { readyToRedirect } = await chrome.storage.local.get("readyToRedirect");
 
-  {
-    "manifest_version": 3,
-    "name": "AUTO FILL",
-    "version": "1.0",
-    "description": "Fills one input field on a page",
-    "permissions": ["activeTab", "webRequest", "webRequestBlocking", "storage", "scripting", "tabs"],
-    "host_permissions": ["<all_urls>"],
-    "background": {"service_worker": "background.js"},
-    "action": {"default_popup": "popup.html"}
-    
-  }
-  
+    if (readyToRedirect) {
+      console.log("Redirect triggered:", details.url);
+      // Reset flag so future requests don't redirect unless triggered again
+      chrome.storage.local.set({ readyToRedirect: false });
+      return { redirectUrl: "https://www31.mutualofomaha.com/ProviderAccess/claimSearch" };
+    }
+  },
+  { urls: ["https://www31.mutualofomaha.com/ProviderAccess/eligibilitySearch"] },  // Match the URL you want to block or redirect
+  ["blocking"]
+);*/
+
+
