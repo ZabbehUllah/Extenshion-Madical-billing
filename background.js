d1bd9903cf5dfada47d6dc808a178f90eb9b83c3
ZabbehUllah <120080824+ZabbehUllah@users.noreply.github.com>
2026-01-28 08:32:24 -0800

    Add manifest version 3 to background.js
    Added manifest version 3 configuration for the extension.

--- background.js
+++ background.js
@@ -1,1 +1,13 @@
 
+  {
+    "manifest_version": 3,
+    "name": "AUTO FILL",
+    "version": "1.0",
+    "description": "Fills one input field on a page",
+    "permissions": ["activeTab", "webRequest", "webRequestBlocking", "storage", "scripting", "tabs"],
+    "host_permissions": ["<all_urls>"],
+    "background": {"service_worker": "background.js"},
+    "action": {"default_popup": "popup.html"}
+    
+  }
+  
